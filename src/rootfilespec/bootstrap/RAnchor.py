8f48118c6b1df88f331d91820304d95d920af7a1
Samantha <sathabbott@gmail.com>
2025-06-12 13:15:44 -0500

    deserialize header envelope contents

--- src/rootfilespec/bootstrap/RAnchor.py
+++ src/rootfilespec/bootstrap/RAnchor.py
@@ -3,8 +3,9 @@ from typing import Annotated
 from rootfilespec.bootstrap.streamedobject import StreamedObject
 from rootfilespec.buffer import DataFetcher
 from rootfilespec.dispatch import DICTIONARY
-from rootfilespec.rntuple import FooterEnvelope, HeaderEnvelope
 from rootfilespec.rntuple.envelope import REnvelopeLink
+from rootfilespec.rntuple.footer import FooterEnvelope
+from rootfilespec.rntuple.header import HeaderEnvelope
 from rootfilespec.rntuple.RLocator import LargeLocator
 from rootfilespec.serializable import serializable
 from rootfilespec.structutil import Fmt
@@ -25,7 +26,12 @@ class ROOT3a3aRNTuple(StreamedObject):
     fMaxKeySize: Annotated[int, Fmt(">Q")]
 
     def get_header(self, fetch_data: DataFetcher) -> HeaderEnvelope:
-        raise NotImplementedError
+        """Reads the RNTuple Header Envelope from the given buffer."""
+        headerLink = REnvelopeLink(
+            self.fLenHeader, LargeLocator(self.fNBytesHeader, self.fSeekHeader)
+        )
+
+        return headerLink.read_envelope(fetch_data, HeaderEnvelope)
 
     def get_footer(self, fetch_data: DataFetcher) -> FooterEnvelope:
         """Reads the RNTuple Footer Envelope from the given buffer."""
